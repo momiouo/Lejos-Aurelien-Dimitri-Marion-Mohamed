194ecd1058c2b523514a628f90b68426d483de9a
Momiouo <47118225+Momiouo@users.noreply.github.com>
2020-09-30 11:49:51 +0200

    # WARNING: head commit changed in the meantime
    Capteur tactile + ultrasons avec setters

--- FirstApp/src/CapteurTactile.java
+++ FirstApp/src/CapteurTactile.java
@@ -1,7 +1,10 @@
+import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.robotics.SampleProvider;
 
 public class CapteurTactile {
 	private boolean pression;
+	private EV3TouchSensor donneesCapteur;
 	
 	public CapteurTactile(Perception perception){
 		pression = perception.getPressionCapteurTactile();
@@ -11,8 +14,14 @@ public class CapteurTactile {
 		return this.pression;
 	}
 	
-	public void setPression(boolean pression) {
-		this.pression = pression;
+	public void setPression() {
+		donneesCapteur = new EV3TouchSensor(SensorPort.S1);
+		final SampleProvider sp = donneesCapteur.getTouchMode();
+		boolean touchValue = false;
+		float [] sample = new float[sp.sampleSize()];
+        sp.fetchSample(sample, 0);
+        touchValue = (sample[0]!=0);
+        this.pression = touchValue;
 	}
 	
 }
--- FirstApp/src/CapteurUltrasons.java
+++ FirstApp/src/CapteurUltrasons.java
@@ -1,5 +1,11 @@
-public class CapteurUltrasons {
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
+
+public class CapteurUltrasons implements SensorPort {
 	private long distance;
+	private EV3UltrasonicSensor donneesCapteur;
+	private SensorPort port;
 	
 	public CapteurUltrasons(Perception perception) {
 		this.distance = perception.getDistanceCapteurUltrasons();
@@ -18,8 +24,33 @@ public class CapteurUltrasons {
 		return this.distance;
 	}
 	
-	public void setDistance(long distance) {
-		this.distance = distance;
+	public void setDistance() {
+		donneesCapteur = new EV3UltrasonicSensor((lejos.hardware.port.Port) this.getPort());
+		final SampleProvider sp = donneesCapteur.getDistanceMode();
+		int distanceValue = 0;
+		float [] sample = new float[sp.sampleSize()];
+		sp.fetchSample(sample, 0);
+        distanceValue = (int)sample[0];
+		this.distance = distanceValue;
+	}
+	
+	public SensorPort getPort() {
+		return this.port;
+	}
+	
+	public void setPort(int numPort) {
+		if (numPort == 1) {
+			this.port=(SensorPort) S1;
+		}
+		else if (numPort == 2) {
+			this.port=(SensorPort) S2;
+		}
+		else if (numPort == 3) {
+			this.port=(SensorPort) S3;
+		}
+		else if (numPort == 4) {
+			this.port=(SensorPort) S4;
+		}
 	}
 	
 	public boolean murOuRobotDetecte() {
