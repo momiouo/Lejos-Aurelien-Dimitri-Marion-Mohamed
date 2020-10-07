9c13aad556e85991112986c93418bb65118f7bc7
Momiouo <47118225+Momiouo@users.noreply.github.com>
2020-10-07 09:52:53 +0200

    Debut développement classe Agent

--- FirstApp/src/Agent.java
+++ FirstApp/src/Agent.java
@@ -1,4 +1,82 @@
+import Controleur.Action;
+import Vue.*;
+import Moteurs.*;
 
 public class Agent {
+	
+	private Action action;
+	private Perception perceptionAct;
+	private Perception perceptionPrec;
+	private AvancerOuReculer avancerOuReculer;
+	private TournerOuPivoter tournerOuPivoter;
+	private CapteurUltrasons capteurUltrasons;
+	private CapteurCouleur capteurCouleur;
+	private CapteurTactile capteurTactile;
+	
+	public Agent() {
+		this.action = new Action();
+		this.perceptionAct = new Perception();
+		this.perceptionPrec = new Perception();
+		//Mettre les bons ports pour les capteurs et les moteurs :
+		this.avancerOuReculer = new AvancerOuReculer();
+		this.tournerOuPivoter = new TournerOuPivoter();
+		this.capteurUltrasons = new CapteurUltrasons();
+		this.capteurCouleur = new CapteurCouleur();
+		this.capteurTactile = new CapteurTactile();
+	}
+	
+	
+	
+	public Action getAction() {
+		return action;
+	}
+	public void setAction(Action action) {
+		this.action = action;
+	}
+	public Perception getPerceptionAct() {
+		return perceptionAct;
+	}
+	public void setPerceptionAct(Perception perceptionAct) {
+		this.perceptionAct = perceptionAct;
+	}
+	public Perception getPerceptionPrec() {
+		return perceptionPrec;
+	}
+	public void setPerceptionPrec(Perception perceptionPrec) {
+		this.perceptionPrec = perceptionPrec;
+	}
+	public AvancerOuReculer getAvancerOuReculer() {
+		return avancerOuReculer;
+	}
+	public void setAvancerOuReculer(AvancerOuReculer avancerOuReculer) {
+		this.avancerOuReculer = avancerOuReculer;
+	}
+	public TournerOuPivoter getTournerOuPivoter() {
+		return tournerOuPivoter;
+	}
+	public void setTournerOuPivoter(TournerOuPivoter tournerOuPivoter) {
+		this.tournerOuPivoter = tournerOuPivoter;
+	}
+	public CapteurUltrasons getCapteurUltrasons() {
+		return capteurUltrasons;
+	}
+	public void setCapteurUltrasons(CapteurUltrasons capteurUltrasons) {
+		this.capteurUltrasons = capteurUltrasons;
+	}
+	public CapteurCouleur getCapteurCouleur() {
+		return capteurCouleur;
+	}
+	public void setCapteurCouleur(CapteurCouleur capteurCouleur) {
+		this.capteurCouleur = capteurCouleur;
+	}
+	public CapteurTactile getCapteurTactile() {
+		return capteurTactile;
+	}
+	public void setCapteurTactile(CapteurTactile capteurTactile) {
+		this.capteurTactile = capteurTactile;
+	}
+	
+	
+	
 
 }
