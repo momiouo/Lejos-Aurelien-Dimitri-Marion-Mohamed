49dee7358a119506bdc1e02dc1df0682ac8f152e
Momiouo <47118225+Momiouo@users.noreply.github.com>
2020-10-08 13:55:57 +0200

    ++

---
+++ FirstApp/src/MainTest/testPinces.java
@@ -0,0 +1,26 @@
+package MainTest;
+
+import Moteurs.Pinces;
+import lejos.hardware.motor.Motor;
+
+public class testPinces {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test des pinces sur le port A");
+		Pinces pinces = new Pinces(Motor.A);
+		
+		System.out.println("On mets les pinces en position initiales");
+		pinces.InitPositionPinces();
+		
+		System.out.println("On ouvre les pinces");
+		pinces.ouverture();
+		
+		System.out.println("On ferme les pinces");
+		pinces.fermeture();
+		
+		System.out.println("Fin du test des pinces");
+	}
+
+}
