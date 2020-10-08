c6b6aa4ac86d83125e6a6628da27961f32230d80
Momiouo <47118225+Momiouo@users.noreply.github.com>
2020-10-08 13:55:44 +0200

    classe Pinces et testPinces réalisées

--- FirstApp/src/Moteurs/Pinces.java
+++ FirstApp/src/Moteurs/Pinces.java
@@ -4,22 +4,33 @@ import lejos.robotics.RegulatedMotor;
 
 public class Pinces { //A tester plus tard
 	
-	private RegulatedMotor moteursPinces;
+	private RegulatedMotor moteurPinces;
+	private boolean pincesOuvertes;
 	
 	public Pinces(RegulatedMotor moteurPinces) {
-		this.moteursPinces = moteurPinces;
+		this.moteurPinces = moteurPinces;
+		this.pincesOuvertes = false;
 	}
 	
-	public void InitPositionPinces() {
-
+	public void InitPositionPinces() {//Pinces en mode ouvert
+		//A tester positionnement des pinces
+		this.moteurPinces.resetTachoCount();//Reset compte tour
+		this.moteurPinces.rotate(0);//mets les pinces à 0 (on imagine c'est fermé)
+		this.moteurPinces.rotate(180);//Ouvre les pinces à 180
+		pincesOuvertes = true;
+		
 	}
 
-	public void ouverture() {
-
+	public void ouverture() {//Ouverture des pinces seulement si pincesOuvertes == false
+		if (!this.pincesOuvertes) {
+			this.moteurPinces.rotate(180);
+		}
 	}
 
-	public void fermeture() {
-
+	public void fermeture() {//Fermeture seulement si pinceOuvertes == true
+		if (this.pincesOuvertes) {
+			this.moteurPinces.rotate(0);
+		}
 	}
 	
 	
